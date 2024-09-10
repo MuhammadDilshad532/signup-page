6befe012da14d35bbaef5fb4a9ac50f10615de85
Dilshad <dilshadahamad532280@gmail.com>
2024-09-10 14:33:28 +0500

    add Go to Home button

--- src/successfullyLogin.jsx
+++ src/successfullyLogin.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 const successfullyLogin = () => {
   return (
     <div>
       <h1 className='text-5xl font-bold text-red-700'>you Successfully Login</h1>
       <h1 className='text-5xl font-bold text-red-700 mt-4'>Thank You for Login Page</h1>
+      <Link to="/Register">
+              <button className='w-48 mb-4 text-[18px] mt-14 rounded-full py-2 transition-colors duration-300 bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white'>Go to Home</button>
+            </Link>
     </div>
   )
 }
