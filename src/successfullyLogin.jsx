f4a7b7a1b85a57d7ad58f99272115de575ff1eef
Dilshad <dilshadahamad532280@gmail.com>
2024-09-10 19:29:12 +0500

    text update

--- src/successfullyLogin.jsx
+++ src/successfullyLogin.jsx
@@ -6,7 +6,7 @@ const successfullyLogin = () => {
       <h1 className='text-5xl font-bold text-red-700'>you Successfully Login</h1>
       <h1 className='text-5xl font-bold text-red-700 mt-4'>Thank You for Login Page</h1>
       <Link to="/Register">
-              <button className='w-48 mb-4 text-[18px] mt-14 rounded-full py-2 transition-colors duration-300 bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white'>Go to Home</button>
+              <button className='w-48 mb-4 text-[18px] mt-14 rounded-full py-2 transition-colors duration-300 bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white'>Back to Register</button>
             </Link>
     </div>
   )
